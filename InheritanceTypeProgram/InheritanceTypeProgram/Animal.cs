734307a225ad7968c53ec9cba5d6d58ee7718240
akshada1709 <gaikwadakshada1709@gmail.com>
2024-11-06 00:11:37 +0530

    Save Program

---
+++ InheritanceTypeProgram/InheritanceTypeProgram/Animal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTypeProgram
+{
+    
+
+        // 1. Single Inheritance
+        public class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Animal is eating.");
+            }
+        }
+
+        public class Cow : Animal
+        {
+            public void Moo()
+            {
+                Console.WriteLine("Cow says moo!");
+            }
+        }
+
+        // 2. Multilevel Inheritance
+        public class Grandfather
+        {
+            public void ShareWisdom()
+            {
+                Console.WriteLine("Grandfather shares wisdom.");
+            }
+        }
+
+        public class Father : Grandfather
+        {
+            public void Teach()
+            {
+                Console.WriteLine("Father is teaching.");
+            }
+        }
+
+        public class Son : Father
+        {
+            public void Play()
+            {
+                Console.WriteLine("Son is playing.");
+            }
+        }
+
+        // 3. Hierarchical Inheritance
+        public class Mother
+        {
+            public void Care()
+            {
+                Console.WriteLine("Mother is caring.");
+            }
+        }
+
+        public class SonOfMother : Mother
+        {
+            public void Study()
+            {
+                Console.WriteLine("Son is studying.");
+            }
+        }
+
+        public class Daughter : Mother
+        {
+            public void Dance()
+            {
+                Console.WriteLine("Daughter is dancing.");
+            }
+        }
+
+     }
+   
--- InheritanceTypeProgram/InheritanceTypeProgram/Program.cs
+++ InheritanceTypeProgram/InheritanceTypeProgram/Program.cs
@@ -11,39 +11,62 @@ namespace InheritanceTypeProgram
         static void Main(string[] args)
         {
 
+            //// Single Inheritance
+            //Parrot parrot = new Parrot();
+            //parrot.FeatherColor = "Green";
+            //parrot.Fly();
+            //parrot.Speak();
+
+            //// Hierarchical Inheritance
+            //Car car = new Car();
+            //car.FuelType = "Petrol";
+            //car.NumberOfSeats = 4;
+            //car.Start();
+            //car.Drive();
+
+            //Bike bike = new Bike();
+            //bike.FuelType = "Electric";
+            //bike.HandlebarType = "Standard";
+            //bike.Start();
+            //bike.Ride();
+
+            //Truck truck = new Truck();
+            //truck.FuelType = "Diesel";
+            //truck.CargoCapacity = 5000;
+            //truck.Start();
+            //truck.Load();
+
+            //// Multilevel Inheritance
+            //Laptop laptop = new Laptop();
+            //laptop.PowerSource = "Battery";
+            //laptop.ProcessorType = "Intel";
+            //laptop.BatteryLife = 8;
+            //laptop.TurnOn();
+            //laptop.Compute();
+            //laptop.PortableUse();
+
+            //Console.ReadLine();
+
+
             // Single Inheritance
-            Parrot parrot = new Parrot();
-            parrot.FeatherColor = "Green";
-            parrot.Fly();
-            parrot.Speak();
+            Cow cow = new Cow();
+            cow.Eat();
+            cow.Moo();
 
-            // Hierarchical Inheritance
-            Car car = new Car();
-            car.FuelType = "Petrol";
-            car.NumberOfSeats = 4;
-            car.Start();
-            car.Drive();
+            // Testing Multilevel Inheritance
+            Son son = new Son();
+            son.ShareWisdom();
+            son.Teach();
+            son.Play();
 
-            Bike bike = new Bike();
-            bike.FuelType = "Electric";
-            bike.HandlebarType = "Standard";
-            bike.Start();
-            bike.Ride();
+            // Testing Hierarchical Inheritance
+            SonOfMother sonOfMother = new SonOfMother();
+            sonOfMother.Care();
+            sonOfMother.Study();
 
-            Truck truck = new Truck();
-            truck.FuelType = "Diesel";
-            truck.CargoCapacity = 5000;
-            truck.Start();
-            truck.Load();
-
-            // Multilevel Inheritance
-            Laptop laptop = new Laptop();
-            laptop.PowerSource = "Battery";
-            laptop.ProcessorType = "Intel";
-            laptop.BatteryLife = 8;
-            laptop.TurnOn();
-            laptop.Compute();
-            laptop.PortableUse();
+            Daughter daughter = new Daughter();
+            daughter.Care();
+            daughter.Dance();
 
             Console.ReadLine();
         }
