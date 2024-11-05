168f1791c7e6a32302d415dc55b654058e25235b
akshada1709 <gaikwadakshada1709@gmail.com>
2024-11-06 00:00:13 +0530

    Add Inheritance code

---
+++ InheritanceTypeProgram/InheritanceTypeProgram/Bird.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTypeProgram
+{
+    // 1. Single Inheritance
+    public class Bird
+    {
+        public string FeatherColor { get; set; }
+
+        public void Fly()
+        {
+            Console.WriteLine("Bird is flying.");
+        }
+    }
+
+    public class Parrot : Bird
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Parrot can mimic sounds.");
+        }
+    }
+
+    // 2. Hierarchical Inheritance
+    public class Vehicle
+    {
+        public string FuelType { get; set; }
+        public int MaxSpeed { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Vehicle is starting.");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public int NumberOfSeats { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving.");
+        }
+    }
+
+    public class Bike : Vehicle
+    {
+        public string HandlebarType { get; set; }
+
+        public void Ride()
+        {
+            Console.WriteLine("Bike is riding.");
+        }
+    }
+
+    public class Truck : Vehicle
+    {
+        public int CargoCapacity { get; set; }
+
+        public void Load()
+        {
+            Console.WriteLine("Truck is loading cargo.");
+        }
+    }
+
+    // 3. Multilevel Inheritance
+    public class ElectronicDevice
+    {
+        public string PowerSource { get; set; }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Electronic device is turning on.");
+        }
+    }
+
+    public class Computer : ElectronicDevice
+    {
+        public string ProcessorType { get; set; }
+
+        public void Compute()
+        {
+            Console.WriteLine("Computer is processing data.");
+        }
+    }
+
+    public class Laptop : Computer
+    {
+        public int BatteryLife { get; set; }
+
+        public void PortableUse()
+        {
+            Console.WriteLine("Laptop can be used portably.");
+        }
+    }
+}
---
+++ InheritanceTypeProgram/InheritanceTypeProgram/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTypeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Single Inheritance
+            Parrot parrot = new Parrot();
+            parrot.FeatherColor = "Green";
+            parrot.Fly();
+            parrot.Speak();
+
+            // Hierarchical Inheritance
+            Car car = new Car();
+            car.FuelType = "Petrol";
+            car.NumberOfSeats = 4;
+            car.Start();
+            car.Drive();
+
+            Bike bike = new Bike();
+            bike.FuelType = "Electric";
+            bike.HandlebarType = "Standard";
+            bike.Start();
+            bike.Ride();
+
+            Truck truck = new Truck();
+            truck.FuelType = "Diesel";
+            truck.CargoCapacity = 5000;
+            truck.Start();
+            truck.Load();
+
+            // Multilevel Inheritance
+            Laptop laptop = new Laptop();
+            laptop.PowerSource = "Battery";
+            laptop.ProcessorType = "Intel";
+            laptop.BatteryLife = 8;
+            laptop.TurnOn();
+            laptop.Compute();
+            laptop.PortableUse();
+
+            Console.ReadLine();
+        }
+    }
+}
