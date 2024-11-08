b06d719154746dfda89e80d046224f90e79ace0f
akshada1709 <gaikwadakshada1709@gmail.com>
2024-11-08 10:37:26 +0530

    add

--- ArrayAssigment1/ArrayAssigment1/ShiftEvenbefOdd.cs
+++ ArrayAssigment1/ArrayAssigment1/ShiftEvenbefOdd.cs
@@ -9,5 +9,47 @@ namespace ArrayAssigment1
     public class ShiftEvenbefOdd
     {
         //9. Shift even before odd 
+       
+        public void ShiftEvenBeforeOdd(int[] arr)
+        {
+            int index = 0;
+
+           
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0) 
+                {
+                   
+                    int temp = arr[index];
+                    arr[index] = arr[i];
+                    arr[i] = temp;
+                    index++;
+                }
+            }
+        }
+
+       
+        public void PrintArray(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    ShiftEvenbefOdd shiftEvenBeforeOdd = new ShiftEvenbefOdd();
+
+        //    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //    Console.WriteLine("Original Array:");
+        //    shiftEvenBeforeOdd.PrintArray(arr);
+
+        //    shiftEvenBeforeOdd.ShiftEvenBeforeOdd(arr);
+        //    Console.WriteLine("Array after shifting even numbers before odd numbers:");
+        //    shiftEvenBeforeOdd.PrintArray(arr);
+
+        //    Console.ReadLine();
+        //}
     }
 }
--- ArrayAssigment1/ArrayAssigment1/ShiftNegBefPositive.cs
+++ ArrayAssigment1/ArrayAssigment1/ShiftNegBefPositive.cs
@@ -9,5 +9,41 @@ namespace ArrayAssigment1
     public class ShiftNegBefPositive
     {
         //10. Shift negative before positive in array
+        public void ShiftNegativeBeforePositive(int[] arr)
+        {
+            int index = 0;
+
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0) 
+                {
+                   
+                    int temp = arr[index];
+                    arr[index] = arr[i];
+                    arr[i] = temp;
+                    index++;
+                }
+            }
+        }
+
+        public void PrintArray(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+        //static void Main(string[] args)
+        //{
+        //    ShiftNegBefPositive shiftNegBefPositive = new ShiftNegBefPositive();
+
+        //    int[] arr = { -1, 2, -3, 4, -5, 6, -7, 8, 9 };
+        //    Console.WriteLine("Original Array:");
+        //    shiftNegBefPositive.PrintArray(arr);
+
+        //    shiftNegBefPositive.ShiftNegativeBeforePositive(arr);
+        //    Console.WriteLine("Array after shifting negative numbers before positive numbers:");
+        //    shiftNegBefPositive.PrintArray(arr);
+
+        //    Console.ReadLine();
+        //}
     }
 }
