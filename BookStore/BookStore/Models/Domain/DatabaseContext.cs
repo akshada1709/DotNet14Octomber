c1f7e30d8d487d4f540f851eb5c086a721494b36
akshada1709 <gaikwadakshada1709@gmail.com>
2024-12-28 15:55:48 +0530

    save

---
+++ BookStore/BookStore/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models.Domain
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Publisher> Publisher { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<General> Genre { get; set; }
+
+    }
+}
---
+++ BookStore/BookStore/Models/Domain/General.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models.Domain
+{
+    public class General
+    {
+        public int Id {  get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
